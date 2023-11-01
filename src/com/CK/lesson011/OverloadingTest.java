cf05adeebfb91b3d8eec25e52714c93a0cf06a00
EXMPLS <dnzhncan.kurtcan@hotmail.com>
2023-11-01 13:43:46 +0300

    Q51 added

---
+++ src/com/CK/lesson011/OverloadingTest.java
@@ -0,0 +1,8 @@
+package com.CK.lesson011;
+
+public class OverloadingTest {
+
+    public static void harfDegistir(String metin, String ilkharf, String ikinciharf){
+        System.out.println(metin.replace(ilkharf,ikinciharf));
+    }
+}
---
+++ src/com/CK/lesson011/Question51.java
@@ -0,0 +1,27 @@
+package com.CK.lesson011;
+
+import java.util.Arrays;
+
+public class Question51 {
+    /*
+    iki tane String değer alıp araya atıp arayı yazdıran fonksiyon
+    ikiden fazla değer alsın ve arayı yazırsın (varargs)
+     */
+
+    public static String araYazma(String deger1, String deger2){
+        String[] dizi = new String[2];
+        dizi[0] = deger1;
+        dizi[1] = deger2;
+        System.out.println(dizi[0]);
+        System.out.println(dizi[1]);
+        System.out.println(Arrays.toString(dizi));
+        for (String string: dizi){
+            System.out.println(string);
+        }
+    }
+
+    public static String araYazma(String...deger){
+
+    }
+
+}
