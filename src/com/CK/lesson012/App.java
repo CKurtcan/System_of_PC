8d419f4294fac650849e928c011d74c23c64bbf9
EXMPLS <dnzhncan.kurtcan@hotmail.com>
2023-11-01 15:41:52 +0300

    Q51 added

--- src/com/CK/lesson012/App.java
+++ src/com/CK/lesson012/App.java
@@ -15,28 +15,52 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
+
         TasKagitMakas oyun = new TasKagitMakas();
         oyun.oyuncuAdi = "Mustafa";
         System.out.println("App main metot ==> " +oyun.secimYap());
 
-    }
 
+
+
+
+
+
+    }
+    /*
     public static void selection(){
         Random random = new Random();
         System.out.println("Kaç kere oynamak istiyorsunuz?");
         int num = new Scanner(System.in).nextInt();
         String[] array = {"Rock","Paper","Scissors"};
-        for (int i = 0; i < num; i++){
+        for (int i = 0; i < num; i++) {
             int choose = random.nextInt(3);
             System.out.println("Lütfen seçim yapınız...");
             String answer = array[choose];
-            System.out.println(answer);
             String game = new Scanner(System.in).nextLine();
-            if (game.equals(answer)){
-                System.out.println("Berabere!!!\nSeçimler: \nSeçiminiz: " +answer +"\nAI'ın Seçimi: " +answer);
+            if (game.equals(answer)) {
+                System.out.println("Berabere!!!\nSeçimler: \nSeçiminiz: " + answer + "\nAI'ın Seçimi: " + answer);
             }
-            if (!game.equals(answer) && game.equals("Paper") && answer.equals("Scissors"))
-                System.out.println("Kaybettiniz!!!\nSeçimler: \nSeçiminiz: " +answer +"\nAI'ın Seçimi: " +answer);
+            if (game.equals("Paper")) {
+                if (answer.equals("Scissors"))
+                    System.out.println("Kaybettiniz!!!\nSeçimler: \nSeçiminiz: " + answer + "\nAI'ın Seçimi: " + answer);
+                if (answer.equals("Rock"))
+                    System.out.println("Kazandınız!!!\nSeçimler: \nSeçiminiz: " + answer + "\nAI'ın Seçimi: " + answer);
+            }
+            if (game.equals("Scissors")) {
+                if (answer.equals("Rock"))
+                    System.out.println("Kaybettiniz!!!\nSeçimler: \nSeçiminiz: " + answer + "\nAI'ın Seçimi: " + answer);
+                if (answer.equals("Paper"))
+                    System.out.println("Kazandınız!!!\nSeçimler: \nSeçiminiz: " + answer + "\nAI'ın Seçimi: " + answer);
+            }
+            if (game.equals("Rock")) {
+                if (answer.equals("Paper"))
+                    System.out.println("Kaybettiniz!!!\nSeçimler: \nSeçiminiz: " + answer + "\nAI'ın Seçimi: " + answer);
+                if (answer.equals("Scissors"))
+                    System.out.println("Kazandınız!!!\nSeçimler: \nSeçiminiz: " + answer + "\nAI'ın Seçimi: " + answer);
+            }
         }
+        System.out.println("Oyunumuzu oynadığınız için teşekkürler");
     }
+     */
 }
